9de4bc9ff94f018a37b45677ecd76c34f51c252b
Sanjyot <in-sanjyot.bandal@Sanjyots-MacBook-Pro.local>
2019-05-06 12:37:59 +0530

    implemented spark streaming word count example

---
+++ src/main/scala/MainApp.scala
@@ -0,0 +1,32 @@
+import org.apache.spark.sql.SparkSession
+
+object MainApp extends App {
+
+  val spark = SparkSession
+    .builder
+    .master("local")
+    .appName("StructuredNetworkWordCount")
+    .getOrCreate()
+
+  import spark.implicits._
+
+  val lines = spark.readStream
+    .format("socket")
+    .option("host", "localhost")
+    .option("port", 9999)
+    .load()
+
+  // Split the lines into words
+  val words = lines.as[String].flatMap(_.split(" "))
+
+  // Generate running word count
+  val wordCounts = words.groupBy("value").count()
+
+  val query = wordCounts.writeStream
+    .outputMode("complete")
+    .format("console")
+    .start()
+
+  query.awaitTermination()
+
+}
